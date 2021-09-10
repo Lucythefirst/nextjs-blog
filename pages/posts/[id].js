9fa727c72ed9f19babb1363cf2323266fd30e135
Lucy <78696563+Lucythefirst@users.noreply.github.com>
2021-09-10 16:25:51 +0100

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,23 +4,6 @@ import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
-}
-
 export default function Post({ postData }) {
   return (
     <Layout>
@@ -37,3 +20,22 @@ export default function Post({ postData }) {
     </Layout>
   )
 }
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+
+
